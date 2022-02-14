e38312ec89fc814e264c569e133fae442c0426bf
AndrewThomae <70494198+AndrewThomae@users.noreply.github.com>
2022-02-14 18:39:11 -0500

    Setting up some classes

--- src/Testing.java
+++ src/Testing.java
@@ -1,5 +1,6 @@
 public class Testing {
     public static void main(String[] args) {
+        //Basic test from command line
         CookieFinder finder = new CookieFinder(args[0], args[2]);
         finder.get_active_cookies();
     }
