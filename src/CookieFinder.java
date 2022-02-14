6b81c4ddc4807254f1a29fcaec71fc47e7cc3b09
AndrewThomae <70494198+AndrewThomae@users.noreply.github.com>
2022-02-14 18:45:40 -0500

    SUCCESSFULLY READS FILE

--- src/CookieFinder.java
+++ src/CookieFinder.java
@@ -18,11 +18,12 @@ public class CookieFinder {
         //Open the file and reader
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader(filename));
+            reader = new BufferedReader(new FileReader("C:\\Users\\drewt\\IdeaProjects\\most_active_cookies\\src\\" + filename));
             String line = reader.readLine();
             //Read every line
             while (line != null) {
                 System.out.println(line);
+                line = reader.readLine();
             }
         } catch (Exception e) {
             System.out.println("ERROR: TROUBLE OPENING FILE");
