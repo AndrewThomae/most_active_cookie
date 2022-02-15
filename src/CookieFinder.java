b7095a59e46b16e68afeaefaad7cce24abc0078b
AndrewThomae <70494198+AndrewThomae@users.noreply.github.com>
2022-02-14 19:10:05 -0500

    Parse the line

--- src/CookieFinder.java
+++ src/CookieFinder.java
@@ -14,6 +14,7 @@ public class CookieFinder {
     //Find all the cookies that fit the date requirement, and return in an array
     public String[] get_active_cookies() {
         String[] cookies = new String[5];
+        int cookieInd = 0;
 
         //Open the file and reader
         BufferedReader reader;
@@ -23,8 +24,22 @@ public class CookieFinder {
             //Read every line
             while (line != null) {
                 System.out.println(line);
+                //Split the line into separate variables
+                int commaInd = line.indexOf(',');
+                String cookie = line.substring(0, commaInd);
+                line = line.substring(commaInd);
+                String cDate = line.substring(1, line.indexOf('T'));
+                line = line.substring(line.indexOf('T') + 1);
+                String strNum = line.substring(0, line.indexOf(':'));
+                int cookNum = Integer.parseInt(strNum);
+
+                System.out.println("Cookie: " + cookie + ", cDate: " + cDate + ",strNum: " + strNum);
+
                 line = reader.readLine();
             }
+
+
+            reader.close();
         } catch (Exception e) {
             System.out.println("ERROR: TROUBLE OPENING FILE");
         }
